62a988dbbb4f711413e9d5787db08797402eb6c0
sminocantero <25sofiabelen@gmail.com>
2025-04-28 11:24:24 -0300

    Agrego personajes Pamela y Espina

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,16 @@ module Lib () where
 
 import Text.Show.Functions ()
 
-doble :: Int -> Int
-doble x = x * 2
+typePersonaje = (String, String, String, Bool, Int)
+personajeEspina :: Personaje
+personajeEspina = ("Espina", "Bola de espina", "Granada de espina", true, 4800)
+
+personajePamela :: Personaje
+personajePamela = ("Pamela", "LLuvia de tuercas sanadoras", "Torreta curativa", false, 9600)
+
+personajes :: [Personaje]
+personajes = [personajeEspina, personajePamela]
+
+bolaEspina :: Int -> Int
+bolaEspina vidaOponente = vidaOponente - 1000
+
